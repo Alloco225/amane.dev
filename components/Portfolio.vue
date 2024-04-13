d974a4518df752fd27e81d73b028314f5c0e2248
Alloco + Ketchup <39595564+Alloco225@users.noreply.github.com>
2024-04-13 02:37:45 +0000

    🎉 Initial Commit: :  🚀 Deploy:

---
+++ components/Portfolio.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <section>
+      <div>
+        <h1>hero text copy</h1>
+        <h2>Hero description</h2>
+        <button>Main action</button>
+      </div>
+      <div>
+        <div>Objet1</div>
+        <div>Objet2</div>
+        <div>Objet3</div>
+      </div>
+    </section>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Portfolio',
+}
+</script>
+
+<style>
+
+</style>
