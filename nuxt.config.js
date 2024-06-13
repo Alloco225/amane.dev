8fbc3aef4fc30ad42935ba494e302fd342691087
Alloco + Ketchup <amanehosanna@gmail.com>
2024-06-13 16:58:26 +0000

    🚧 Work in Progress:  trying prime configuration method

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,14 +8,30 @@ export default {
       { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+    { rel: "stylesheet", href: "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css", },
+    {
+      rel: 'stylesheet', type: 'text/css', media: 'screen',
+      href: 'https://unpkg.com/webcam-easy/demo/style/webcam-demo.css',
+    },
+    { rel: 'stylesheet', type: 'text/css', media: 'screen', href: 'style/virtual-glasses.css', },
+    ],
     script: [
 
-    { src:"https://unpkg.com/@tensorflow/tfjs-core@2.4.0/dist/tf-core.min.js" },  
-    { src:"https://unpkg.com/@tensorflow/tfjs-converter@2.4.0/dist/tf-converter.min.js" },  
-    { src:"https://unpkg.com/@tensorflow/tfjs-backend-webgl@2.4.0/dist/tf-backend-webgl.min.js" },  
-    { src:"https://unpkg.com/@tensorflow/tfjs-backend-cpu@2.4.0/dist/tf-backend-cpu.min.js" },  
-    { src:"https://unpkg.com/@tensorflow-models/face-landmarks-detection@0.0.1/dist/face-landmarks-detection.min.js" }, 
+      { src: "https://unpkg.com/@tensorflow/tfjs-core@2.4.0/dist/tf-core.min.js" },
+      { src: "https://unpkg.com/@tensorflow/tfjs-converter@2.4.0/dist/tf-converter.min.js" },
+      { src: "https://unpkg.com/@tensorflow/tfjs-backend-webgl@2.4.0/dist/tf-backend-webgl.min.js" },
+      { src: "https://unpkg.com/@tensorflow/tfjs-backend-cpu@2.4.0/dist/tf-backend-cpu.min.js" },
+      { src: "https://unpkg.com/@tensorflow-models/face-landmarks-detection@0.0.1/dist/face-landmarks-detection.min.js" },
+      //
+      { src: "//code.jquery.com/jquery-3.3.1.min.js", },
+      { src: "https://unpkg.com/@tensorflow/tfjs-core@2.4.0/dist/tf-core.min.js", },
+      { src: "https://unpkg.com/@tensorflow/tfjs-converter@2.4.0/dist/tf-converter.min.js", },
+      { src: "https://unpkg.com/@tensorflow/tfjs-backend-webgl@2.4.0/dist/tf-backend-webgl.min.js", },
+      { src: "https://unpkg.com/@tensorflow/tfjs-backend-cpu@2.4.0/dist/tf-backend-cpu.min.js", },
+      { src: "https://unpkg.com/@tensorflow-models/face-landmarks-detection@0.0.1/dist/face-landmarks-detection.min.js", },
+      { type: "text/javascript", src: "https://unpkg.com/webcam-easy/dist/webcam-easy.min.js", },
+      { type: "text/javascript", src: "js/webcam-ui-lib.js", },
       // {
       //   src: '/face-api/face-api.js'
       // },
---
+++ plugins/3d/main_old.js
@@ -0,0 +1,69 @@
+// import { World } from './World/World.js';
+
+// let world;
+// const videoElement = document.querySelector('#webcam');
+
+// async function initWebcamFeed(){
+//     const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+//     videoElement.srcObject = stream;
+
+
+// }
+
+// async function main() {
+//   console.log("main");
+
+//   await initWebcamFeed();
+
+//   // Get a reference to the container element
+//   const container = document.querySelector('#scene-container');
+
+//   // 1. Create an instance of the World app
+//   world = new World(container);
+
+
+//   /*
+//   // produce a single frame (render on demand)
+//   world.render();
+//   */
+
+//   // complete async tasks
+//   await world.init();
+
+//   await world.initAR(videoElement);
+
+//   world.executeAR(videoElement);
+//   // start the loop (produce a stream of frames)
+//   world.start();
+
+//   //
+
+//   // setUpEventListeners();
+// }
+
+
+// function setUpEventListeners(){
+
+//   let button = document.querySelector('#start-btn')
+//   button?.addEventListener('click', launchRender)
+
+//   document.querySelector('#focusNext-btn').addEventListener('click', focusNext)
+// }
+
+// function launchRender(){
+
+//   console.log("clicked start");
+//   // main();
+// }
+
+// function focusNext(){
+//   console.log("focusNext")
+//   world.focusNext();
+// }
+
+// main().catch((err) => {
+//   console.error('xx main', err);
+// });
+
+
+// // export default { main }
